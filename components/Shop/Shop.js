ef5c8a91c11da6095ae963792c4e2661874ded80
blaine young <blaine.young3553@gmail.com>
2020-09-22 23:26:16 -0400

    added order modal and started adding react hooks for order form

--- components/Shop/Shop.js
+++ components/Shop/Shop.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import { setLightEstimationEnabled } from 'expo/build/AR';
+import React, { useState } from 'react';
 import {ScrollView, Text, StyleSheet, TextInput, Modal, View} from 'react-native';
 import {Card, Button} from 'react-native-elements';
 
 const Shop = () => {
+
+    const [showOrderModal, toggleOrderModal] = useState(false);
+
+    const [showThanksModal, toggleThanksModal] = useState(false);
+
+    const [fName, setFName] = useState('');
+    const [lName, setLName] = useState('');
+    const [email, setEmail] = useState('')
 
     return(
 
@@ -20,8 +29,129 @@ const Shop = () => {
                         }}
                         titleStyle={{
                             color:"#5637DD"
-                        }}/>    
+                        }}
+                        onPress={() => {toggleOrderModal(!showOrderModal)}}/>    
             </Card>
+            <Card
+                image={require('../images/salad.jpg')}>
+                <Text style={styles.cardHeader}>Salad</Text>
+                <Text style={styles.cardText}>Freshly cut lettuce topped with home grown tomatos, garden fresh carrots, and our signature ranch dressing.</Text>
+                <Text style={styles.cardPrice}>$4.99</Text>
+                <Button title="Order Now"
+                        style={styles.cardButton}
+                        buttonStyle={{
+                            backgroundColor: '#e2adf2'
+                        }}
+                        titleStyle={{
+                            color:"#5637DD"
+                        }}
+                        onPress={() => {toggleOrderModal(!showOrderModal)}}/>    
+            </Card>
+            <Card
+                image={require('../images/burger.jpg')}>
+                <Text style={styles.cardHeader}>Burger</Text>
+                <Text style={styles.cardText}>Mouth drooling angus burger on two buns topped with fresh lettuce, tomato, ketchup, and onions.</Text>
+                <Text style={styles.cardPrice}>$8.99</Text>
+                <Button title="Order Now"
+                        style={styles.cardButton}
+                        buttonStyle={{
+                            backgroundColor: '#e2adf2'
+                        }}
+                        titleStyle={{
+                            color:"#5637DD"
+                        }}
+                        onPress={() => {toggleOrderModal(!showOrderModal)}}/>    
+            </Card>
+            <Modal
+                style={styles.modal} 
+                visible={showOrderModal}
+                animationType="slide"
+                onRequestClose={() => {
+                    toggleOrderModal(!showOrderModal)
+                }}>
+                <View style={styles.modal}>
+                <Button
+                    onPress={() => {
+                        toggleOrderModal(!showOrderModal);
+                    }}
+                    color='#5637DD'
+                    title='Close'
+                    style={styles.modalButton}
+                    buttonStyle={{
+                        backgroundColor: '#574AE2'
+                    }}/>
+                    <ScrollView style={{flex: 1}}>
+                        <Text style={styles.pageHeader}>Order!</Text>
+                        <TextInput
+                        style={styles.fName}
+                        placeholder="Enter your first name"
+                        style={styles.firstInput}
+                        onChangeText={value => setFName(value)}/>
+                        <TextInput
+                        placeholder="Enter your last name"
+                        style={styles.input}
+                        onChangeText={value => setLName(value) }/>
+                        <TextInput
+                        placeholder="Enter your email"
+                        style={styles.input}
+                        onChangeText={value => setEmail(value) }/>
+                        <TextInput
+                        placeholder="Enter your address"
+                        style={styles.input}
+                        onChangeText={value => this.setState({address: value})}/>
+                        <TextInput
+                        placeholder="Enter your city"
+                        style={styles.input}
+                        onChangeText={value => this.setState({city: value})}/>
+                        <TextInput
+                        placeholder="Enter your state"
+                        style={styles.input}
+                        onChangeText={value => this.setState({state: value})}/>
+                        <TextInput
+                        placeholder="Enter your zipcode"
+                        style={styles.input}
+                        onChangeText={value => this.setState({zipcode: value})}/>
+                        <TextInput
+                        placeholder="Enter how many pizzas"
+                        style={styles.input}
+                        onChangeText={value => this.setState({months: value})}/>
+                        <TextInput
+                        placeholder="Enter how many salads"
+                        style={styles.input}
+                        onChangeText={value => this.setState({months: value})}/>
+                        <TextInput
+                        placeholder="Enter how many burgers"
+                        style={styles.input}
+                        onChangeText={value => this.setState({months: value})}/>
+                        <TextInput
+                        placeholder="Enter your card type"
+                        style={styles.input}
+                        onChangeText={value => this.setState({cardType: value})}/>
+                        <TextInput
+                        placeholder="Enter your card number"
+                        style={styles.input}
+                        onChangeText={value => this.setState({cardNum: value})}/>
+                        <TextInput
+                        placeholder="Enter your card's expiration date"
+                        style={styles.input}
+                        onChangeText={value => this.setState({cardExp: value})}/>
+                        <TextInput
+                        placeholder="Enter your card's security number"
+                        style={styles.input}
+                        onChangeText={value => this.setState({cardSec: value})}/>
+                        <Button style={styles.submit} title="Submit" 
+                            buttonStyle={{
+                                backgroundColor: '#E2ADF2'
+                            }} 
+                            titleStyle={{
+                                color: '#222A68'
+                            }}
+                            onPress={() => {
+                                toggleThanksModal(!showThanksModal)
+                            }}/>
+                    </ScrollView>
+                </View>
+            </Modal>
         </ScrollView>
 
     );
@@ -61,17 +191,17 @@ const styles = StyleSheet.create({
     },
     firstInput: {
         color: '#654597',
-        height: 50,
+        height: 40,
         marginLeft: 'auto',
         marginRight: 'auto',
         marginBottom: 12,
-        marginTop: 12,
+        marginTop: 8,
         borderWidth: 2,
         width: 250
     },
     input: {
         color: '#654597',
-        height: 50,
+        height: 40,
         marginLeft: 'auto',
         marginRight: 'auto',
         marginBottom: 12,
@@ -86,7 +216,8 @@ const styles = StyleSheet.create({
     },
     modal: { 
         justifyContent: 'center',
-        margin: 20
+        margin: 20,
+        flex: 1
     },
     modalButton: {
         marginTop: 48
